b3b848fc2d7683ed2a0a263786aaa4648d4510b5
moke-square <mekoninmoke@gmail.com>
2022-09-08 07:57:46 -0700

    Helllo, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,8 @@
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
-
-int main(void)  
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
