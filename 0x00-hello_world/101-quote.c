aa31a893c9ba6ebf3503041913b481cf08e87080
moke-square <mekoninmoke@gmail.com>
2022-09-09 20:26:11 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Entry point
- * Return:1
+ * Description: quotes
+ * Return: 1
  *
  **/
 int main(void)
-
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
