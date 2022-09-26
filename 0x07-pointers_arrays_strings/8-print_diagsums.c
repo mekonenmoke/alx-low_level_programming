b5a8467b42fb1e8d667f0de31ef86e225fe38d4f
moke-square <mekoninmoke@gmail.com>
2022-09-26 10:47:13 -0700

    updated

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - a function that prints the sum of the 
+ * print_diagsums - a function that prints the sum of the
  * two diagonals of a square matrix of integers.
  * @a: 2d array of int types
  * @size: size of array (square)
