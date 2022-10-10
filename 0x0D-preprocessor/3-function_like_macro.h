ed77a0b289965c66d664b51da0efc72801f5f08d
moke-square <mekoninmoke@gmail.com>
2022-10-10 14:33:02 -0700

    computes the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,8 +4,6 @@
 /*
  * File: 3-function_like_macro.h
  * Auth: Mekonen Moke Wagara
- * Desc: Header file that defines a function-like macro ABS(x)
- *      that computes the absolute value of a number x.
  */
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
 #endif
